1535f5a94e326a9b3e50acb7f796e6fe0c37e295
Jaromír Beneš <jaromir.benes@gmail.com>
2025-09-28 15:14:49 +0200

    Fixed typo

--- src/documark/__init__.py
+++ src/documark/__init__.py
@@ -1,5 +1,5 @@
 
-from .documentation import document_namespaces, document.namespace
+from .documentation import document_namespaces, document_namespace
 from .introspection import reference, no_reference, delete_documark_attributes
 from .inspection import list_documented_methods, list_undocumented_methods
 
